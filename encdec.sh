d3a0217f9ff7b1c92cb3e7675fd6920b26561887
NikhilMTomy <nikhilmtomy@gmail.com>
2017-04-28 12:02:48 +0000

    Removed a bug from endec.sh

--- encdec.sh
+++ encdec.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 if [ "$#" -eq 2 ]; then
+	dname=`dirname "$2"`
+	fname=`basename "$2"`
 	if [ "$1" == "e" ]; then
-		name=`./enc.exe e "$2"`
-		openssl enc --aes-256-cbc -in "$2" -out "$name"
+		echo ""
+		echo "Encrypting..."
+		name=`./work.exe e "$fname"`
+		openssl enc --aes-256-cbc -in "$2" -out "$dname"/"$name"
+		echo "Finished"
+		echo ""
 	elif [ "$1" == "d" ]; then
-		name=`./enc.exe d "$2"`
-		openssl enc -d --aes-256-cbc -in "$2" -out "$name"
+		echo ""
+		echo "Decrypting..."
+		name=`./work.exe d "$fname"`
+		openssl enc -d --aes-256-cbc -in "$2" -out "$dname"/"$name"
+		echo "Finished"
+		echo ""
 	else
 		./help.sh
 		exit
